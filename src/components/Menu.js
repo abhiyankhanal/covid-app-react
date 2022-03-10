fa6ebda44a7a6caabb9a3996ba4e58345974ce01
Abhiyan Khanal <abhiyankhanal@SPMBP146.local>
2022-03-10 15:29:11 +0545

    init

---
+++ src/components/Menu.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import "./Menu.css";
+export class Menu extends Component {
+  render() {
+    return (
+      <div className="wrap">
+        <div>Covid-19 App</div>
+        <div>Home</div>
+        <div>About-Team-Mars</div>
+       
+      </div>
+    )
+  }
+}
+
+export default Menu
