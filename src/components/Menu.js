04210fb1b4a76f59cecdea478418044a1ddbee09
Abhiyan Khanal <abhiyankhanal@SPMBP146.local>
2022-03-17 11:40:37 +0545

    final

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react'
-// import About from './About';
-import PropTypes from "prop-types";
+
 import {Link} from 'react-router-dom';
 import "./Menu.css";
 export class Menu extends Component {
   render() {
-    const { match, location, history } = this.props;
-    console.log(`history is ${history}`)
+   
+  
     return (
       <div className="wrap">
         <Link className="my-div" to="/" >CovidData</Link>
